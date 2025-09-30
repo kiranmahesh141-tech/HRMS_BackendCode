89ad8a2f0abffbf1a79e628897adcc086a030412
Sai Kumar <saikumarduppada23@gmail.com>
2025-09-30 15:20:48 +0530

    now updated successfully

--- src/main/java/com/HRMSdemo/controller/HRController.java
+++ src/main/java/com/HRMSdemo/controller/HRController.java
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.HRMSdemo.dto.Users;
 import com.HRMSdemo.service.HRService;
-
+@RestController
 @RequestMapping("/hr")
 @CrossOrigin(origins = "http://localhost:3000")
 public class HRController {
