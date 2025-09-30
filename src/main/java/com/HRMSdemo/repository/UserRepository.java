71cbc77dbc225b0f0b9d735629f05ac5addf2a44
Sai Kumar <saikumarduppada23@gmail.com>
2025-09-30 14:44:57 +0530

    Total updated

--- src/main/java/com/HRMSdemo/repository/UserRepository.java
+++ src/main/java/com/HRMSdemo/repository/UserRepository.java
@@ -3,8 +3,7 @@ package com.HRMSdemo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import com.HRMSdemo.dto.Users;
 
@@ -17,16 +16,16 @@ public interface UserRepository extends JpaRepository<Users, String> {
 
 	Users findByEmail(String email);
 
-	@Query("SELECT u FROM Users u WHERE u.status = :status")
-	List<Users> searchByStatus(@Param("status") String status);
-
-	@Query("SELECT u FROM Users u WHERE LOWER(u.fullName) LIKE LOWER(CONCAT('%', :name, '%'))")
-	List<Users> searchByname(@Param("name") String name);
-
-	@Query("SELECT u FROM Users u WHERE str(u.phone) LIKE CONCAT('%', :phone, '%')")
-	List<Users> searchByPhone(@Param("phone") String phone);
 	
-	@Query("SELECT u FROM Users u WHERE LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%'))")
-	List<Users> searchByEmail(@Param("email") String email);
+	List<Users> searchByStatus(String status);
+
+	
+	List<Users> searchByname(String name);
+
+	
+	List<Users> searchByPhone(String phone);
+	
+	
+	List<Users> searchByEmail(String email);
 
 }
